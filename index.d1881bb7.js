d2fbd20023f85db4660e1d10c536e99f7f331cd8
Ostap Hedz <lordanvil18@gmail.com>
2024-03-31 12:25:48 +0200

    make build

---
+++ index.d1881bb7.js
@@ -0,0 +1,2 @@
+const l=document.querySelectorAll(".gallery__slider__link");l.addEventListener("click",()=>{l.classList.contains("gallery__slider__link--active")?l.classList.remove("gallery__slider__link--active"):l.classList.add("gallery__slider__link--active")});
+//# sourceMappingURL=index.d1881bb7.js.map
